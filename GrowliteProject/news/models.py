566f46842cf208e9aafe856b13154365f450bc79
hellypatel691 <hellypatel343@gmail.com>
2026-02-01 12:48:41 +0530

    Delete GrowliteProject directory
    adding new and better

--- GrowliteProject/news/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class News(models.Model):
-    headline = models.CharField(max_length=200)
-    body = models.TextField()
-    date = models.DateField()
